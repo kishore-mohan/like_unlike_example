018f364209ac64e70199b5898fe2a6a3ff44041b
Kishore Mohan <kishorevlrtn@gmail.com>
2014-12-18 18:32:34 +0530

    testing code #1
    testing #1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:vote_items, :show, :edit, :update, :destroy]
+  before_action :set_post, only: [:vote_items, :show, :edit, :update, :destroy] 
 
   before_action :authenticate_user!, except:[:index]
 
